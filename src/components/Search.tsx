32ffcb632c3f193d8b9e4a55a6dfc841342208fe
williamzmacdonald <33135172+williamzmacdonald@users.noreply.github.com>
2021-06-03 01:17:42 -0500

    Get octokit working for simple github search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,38 @@
+import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
-import React from 'react';
+import { Endpoints } from '@octokit/types';
+import { Octokit } from '@octokit/rest';
+
+type listSearchReposParameters = Endpoints['GET /search/repositories']['response'];
 
 const Search = (): JSX.Element => {
-    const submitHandler = (e: React.FormEvent) => {
+    const [searchResults, setSearcResults] = useState<listSearchReposParameters>();
+    const octokit = new Octokit();
+
+    const submitHandler = async (e: React.FormEvent) => {
         // fetch some stuff
         e.preventDefault();
         const target = e.target as typeof e.target & {
             search: { value: string };
         };
-        console.log(target.search.value);
+        // console.log(target.search.value);
+        const results = await octokit.request('GET /search/repositories', {
+            q: target.search.value,
+        });
+        setSearcResults(results);
     };
 
+    const results = searchResults?.data.items.map(repo => (
+        <p key={repo.node_id}>{repo.name}</p>
+    ));
+
     return (
-        <form onSubmit={submitHandler}>
-            <TextField label='Search' name='search'/>
-        </form>
+        <>
+            <form onSubmit={submitHandler}>
+                <TextField label='Search' name='search'/>
+            </form>
+            {results}
+        </>
     );
 }
 
